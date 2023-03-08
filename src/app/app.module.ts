bc5e6f46f57effc67c69cd207d2f940fce6384bb
Tomasz Smoczyński <tomeksmo@gmail.com>
2023-03-08 10:59:02 +0100

    Initial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './core/components/footer/footer.component';
+import { HeaderComponent } from './core/components/header/header.component';
+import { HomeComponent } from './home/home.component';
+import { ModalComponent } from './shared/components/modal/modal.component';
+import { SliderComponent } from './shared/components/slider/slider.component';
+import { SpinnerComponent } from './shared/components/spinner/spinner.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    HeaderComponent,
+    HomeComponent,
+    ModalComponent,
+    SliderComponent,
+    SpinnerComponent,
   ],
   imports: [
     BrowserModule,
