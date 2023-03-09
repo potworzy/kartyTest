1b07cd357e0064e35df3c0cfd0d44f35da729d03
Tomasz Smoczyński <tomeksmo@gmail.com>
2023-03-09 22:03:20 +0100

    doany spinner oraz interceptor dla połączeń uruchamiający spinner

--- src/app/shared/components/spinner/spinner.component.ts
+++ src/app/shared/components/spinner/spinner.component.ts
@@ -1,13 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { SpinnerService } from './spinner.service';
 
 @Component({
   selector: 'app-spinner',
   templateUrl: './spinner.component.html',
-  styleUrls: ['./spinner.component.scss']
+  styleUrls: ['./spinner.component.scss'],
+  encapsulation: ViewEncapsulation.ShadowDom
 })
 export class SpinnerComponent implements OnInit {
 
-  constructor() { }
+  constructor(public spinnerService: SpinnerService) { }
 
   ngOnInit(): void {
   }
